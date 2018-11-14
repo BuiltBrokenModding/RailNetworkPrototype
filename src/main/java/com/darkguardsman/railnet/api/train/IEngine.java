cff1206bc47a0450f56032a3ec4c66bea089cd13
Robert WC Seifert <rseifert.phone@gmail.com>
2018-11-14 13:31:10 -0500

    Started on API

---
+++ src/main/java/com/darkguardsman/railnet/api/train/IEngine.java
@@ -0,0 +1,8 @@
+package com.darkguardsman.railnet.api.train;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 11/14/18.
+ */
+public interface IEngine {
+}
