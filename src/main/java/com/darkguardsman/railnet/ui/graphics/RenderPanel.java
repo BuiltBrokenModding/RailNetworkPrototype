8397391bdb7d34295c088000c4b5890b79f19e9b
Robert WC Seifert <rseifert.phone@gmail.com>
2018-11-16 14:47:19 -0500

    Fixed panel being flipped, Should now be in the correct X & Y

--- src/main/java/com/darkguardsman/railnet/ui/graphics/RenderPanel.java
+++ src/main/java/com/darkguardsman/railnet/ui/graphics/RenderPanel.java
@@ -39,6 +39,10 @@ public class RenderPanel extends JPanel {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
+        // Traslate the Origin and flip the co-ordinate space
+        g2.translate(getWidth() - PAD, getHeight() - PAD);
+        g2.scale(-1, -1);
+
         drawBorder(g2);
         rendersToRun.forEach(render -> render.draw(g2, this));
     }
