b2104ef290ca981582b38ef55938c5e3e8dacec4
Shovinus <baileyjoseph87@hotmail.com>
2018-11-17 01:28:21 +0000

    Create MouseMotionListenerCurve.java

---
+++ src/main/java/com/darkguardsman/railnet/ui/graphics/MouseMotionListenerCurve.java
@@ -0,0 +1,45 @@
+package com.darkguardsman.railnet.ui.graphics;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+import com.darkguardsman.railnet.lib.Pos;
+import com.darkguardsman.railnet.ui.graphics.rail.RailRenderUtil;
+import com.darkguardsman.railnet.ui.graphics.render.PlotPointRender;
+
+/**
+ * Forces a component to keep a size that is 1:1
+ *
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 4/12/2018.
+ */
+public class MouseMotionListenerCurve implements MouseMotionListener
+{
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		RenderPanel panel = (RenderPanel)arg0.getSource();
+		
+		double lWidth = panel.lowerBound.getWidth();
+		double uHeight = panel.upperBound.getHeight();
+
+		double bWidth = panel.upperBound.getWidth() - lWidth;
+		double bHeight = uHeight - panel.lowerBound.getHeight();
+
+		double gx = lWidth + (((double)arg0.getX()/panel.getWidth())*bWidth);
+		double gz = uHeight - (((double)arg0.getY()/panel.getHeight())*bHeight);	
+
+		System.out.println("x:" + gx);
+		System.out.println("y:" + gz);
+		panel.clear();
+		RailRenderUtil.generateRail((PlotPointRender)panel.rendersToRun.get(3), new Pos(0,0,0), new Pos(-gx,0,gz), 0, 180, true);
+		panel.repaint();
+	}
+
+}
