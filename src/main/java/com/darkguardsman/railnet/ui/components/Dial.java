9cd9b04db1772c94c847cd5c9301709e30569024
Robert WC Seifert <rseifert.phone@gmail.com>
2018-11-20 09:44:00 -0500

    Added user input for dial

--- src/main/java/com/darkguardsman/railnet/ui/components/Dial.java
+++ src/main/java/com/darkguardsman/railnet/ui/components/Dial.java
@@ -2,14 +2,26 @@ package com.darkguardsman.railnet.ui.components;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Dial extends JPanel {
+public class Dial extends JPanel implements ComponentListener {
 
     protected final List<Integer> dialPositions = new ArrayList();
 
     protected int dialPosition = 0;
+
+    protected int drawAreaSize;
+    protected int dialOutsideSize;
+
+    protected int pad = 3;
+
+    protected int centerX;
+    protected int centerY;
+
+    protected float dialFaceScale = 0.8f;
 
     public Dial() {
         this(100);
@@ -20,6 +32,8 @@ public class Dial extends JPanel {
         setPreferredSize(new Dimension(size, size));
         setMaximumSize(new Dimension(size, size));
         //TODO create mouse listener for clicks on dial positions to set position
+        addMouseListener(new DialMouseListener(this));
+        addComponentListener(this);
     }
 
     @Override
@@ -31,58 +45,65 @@ public class Dial extends JPanel {
         //Draw boarder
         g2.drawRect(1, 1, getWidth() - 2, getHeight() - 2);
 
-        //Get size for dial
-        final int drawAreaSize = getWidth() > getHeight() ? getHeight() : getWidth();
-        final int pad = 3;
-
-        int centerX = drawAreaSize / 2;
-        int centerY = drawAreaSize / 2;
-
-        //Adjust size for padding
-        int size = drawAreaSize - pad * 2;
-
-        //Get radius
-        int radius = size / 2;
-
         //Draw dial background
-        g2.setColor(Color.BLUE);
-        g2.fillOval(centerX - radius, centerY - radius, size, size);
+        drawCircle(g2, Color.BLUE, centerX, centerY, dialOutsideSize);
 
         //Draw dial positions
         g2.setColor(Color.GRAY);
-        for (Integer angle : dialPositions) {
-            int x = (int) (radius * Math.cos(Math.toRadians(angle)));
-            int y = (int) -(radius * Math.sin(Math.toRadians(angle)));
-
-            g2.drawLine(centerX, centerY, centerX + x, centerY + y);
+        for (int i = 0; i < dialPositions.size(); i++) {
+            drawLineAtAngle(g2, centerX, centerY, getAngleVisually(i), dialOutsideSize / 2);
         }
 
-        //Draw dial
-        size = (int) (size * 0.8);
-        radius = size / 2;
-
-        //Draw dial background
-        g2.setColor(Color.BLACK);
-        g2.fillOval(centerX - radius, centerY - radius, size, size);
+        //Draw dial face
+        drawCircle(g2, Color.BLACK, centerX, centerY, (int) (dialOutsideSize * dialFaceScale));
 
         //Draw line on dial face
-        int angle = dialPositions.get(dialPosition);
         g2.setColor(Color.GREEN);
-        int x = getX(radius, angle);
-        int y = getY(radius, angle);
-
-        g2.drawLine(centerX, centerY, centerX + x, centerY + y);
-
+        drawLineAtAngle(g2, centerX, centerY, getAngleVisually(dialPosition), dialOutsideSize / 2);
     }
 
-    private int getX(int radius, int angle)
-    {
+    public int getAngleVisually(int index) {
+        return dialPositions.get(index);
+    }
+
+    private void drawLineAtAngle(Graphics2D g2, int centerX, int centerY, int angle, int size) {
+        int x = getX(size, angle);
+        int y = getY(size, angle);
+
+        g2.drawLine(centerX, centerY, centerX + x, centerY + y);
+    }
+
+    private void drawCircle(Graphics2D g2, Color color, int centerX, int centerY, int size) {
+        g2.setColor(color);
+        drawCircle(g2, centerX, centerY, size);
+    }
+
+    private void drawCircle(Graphics2D g2, int centerX, int centerY, int size) {
+        int radius = size / 2;
+        g2.fillOval(centerX - radius, centerY - radius, size, size);
+    }
+
+    private int getX(int radius, int angle) {
         return (int) (radius * Math.cos(Math.toRadians(angle)));
     }
 
-    private int getY(int radius, int angle)
-    {
+    private int getY(int radius, int angle) {
         return (int) -(radius * Math.sin(Math.toRadians(angle)));
+    }
+
+    public int getAngle() {
+        return dialPositions.get(dialPosition);
+    }
+
+    public void setDialPosition(int index) {
+        if (index >= 0 && index < dialPositions.size()) {
+            dialPosition = index;
+            System.out.println("Dial#setDialPosition(" + index + ") - dial set to " + getAngle());
+            repaint();
+        } else {
+            System.out.println("Dial#setDialPosition(" + index + ") - Error, Index is outside the range of values");
+            new RuntimeException("trace").printStackTrace();
+        }
     }
 
     public Dial addPosition(int angle) {
@@ -90,5 +111,30 @@ public class Dial extends JPanel {
             this.dialPositions.add(angle);
         repaint();
         return this;
+    }
+
+    @Override
+    public void componentResized(ComponentEvent e) {
+        drawAreaSize = getWidth() > getHeight() ? getHeight() : getWidth();
+
+        centerX = drawAreaSize / 2;
+        centerY = drawAreaSize / 2;
+
+        dialOutsideSize = drawAreaSize - pad * 2;
+    }
+
+    @Override
+    public void componentMoved(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentShown(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentHidden(ComponentEvent e) {
+
     }
 }
---
+++ src/main/java/com/darkguardsman/railnet/ui/components/DialMouseListener.java
@@ -0,0 +1,84 @@
+package com.darkguardsman.railnet.ui.components;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 11/20/18.
+ */
+public class DialMouseListener implements MouseListener {
+    public final Dial dial;
+
+    public DialMouseListener(Dial dial) {
+        this.dial = dial;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+        if (mouseEvent.getSource() == dial) {
+            int x = mouseEvent.getX();
+            int y = mouseEvent.getY();
+
+            //Check that mouse clicked near or over dial face
+            if (x >= dial.pad && y >= dial.pad && x < (dial.drawAreaSize - dial.pad) && y < (dial.drawAreaSize - dial.pad)) {
+
+                //Convert position to angle
+                int deltaX = x - dial.centerX;
+                int deltaY = y - dial.centerY;
+                double rad = Math.atan2(deltaY, deltaX);
+                double clickAngle = -rad * (180 / Math.PI);
+
+                //Wrap to 360
+                while (clickAngle < 0) {
+                    clickAngle += 360;
+                }
+                while (clickAngle > 360) {
+                    clickAngle -= 360;
+                }
+
+                System.out.println("Angle: " + rad + " " + clickAngle);
+
+                //Find best match angle
+                int distance = 360;
+                int dialIndex = 0;
+                for (int i = 0; i < dial.dialPositions.size(); i++) {
+                    int selectionAngle = dial.getAngleVisually(i);
+                    int diff = getAngleDelta(selectionAngle, (int)clickAngle);
+                    if (diff < distance) {
+                        distance = diff;
+                        dialIndex = i;
+                    }
+                }
+
+                //Set position
+                dial.setDialPosition(dialIndex);
+            }
+        }
+    }
+
+    private int getAngleDelta(int a, int b) {
+        return Math.min((a - b + 360) % 360, (b - a + 360) % 360);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        //Not used
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //Not used
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        //Not used
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //Not used
+    }
+}
