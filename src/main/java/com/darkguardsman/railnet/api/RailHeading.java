5a55cb05956971b755bd6db0b8abd221c12f5451
Robert WC Seifert <rseifert.phone@gmail.com>
2018-11-21 11:09:13 -0500

    Added possible rail headings to RailHeading Enum

--- src/main/java/com/darkguardsman/railnet/api/RailHeading.java
+++ src/main/java/com/darkguardsman/railnet/api/RailHeading.java
@@ -1,9 +1,14 @@
 package com.darkguardsman.railnet.api;
 
 import com.darkguardsman.railnet.lib.MathHelpers;
-import com.darkguardsman.railnet.lib.utils.SegmentHelper;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
+ * Enum of support headings for rails
+ *
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 11/14/18.
  */
@@ -17,14 +22,107 @@ public enum RailHeading {
     WEST(-1, 0, 270),
     NORTH_WEST(-1, -1, 315);
 
+    /**
+     * Offset for the X axis
+     */
     public final int offsetX;
+    /**
+     * Offset for the Z axis
+     */
     public final int offsetZ;
+    /**
+     * Angle of the heading
+     */
     public final int angle;
+
+    //Possible headings for the value to proceed
+    private List<RailHeading> possibleHeadings;
+
+    static {
+        for (RailHeading heading : values()) {
+            heading.possibleHeadings = Collections.unmodifiableList(Arrays.asList(
+                    heading.left().left(),
+                    heading.left(),
+                    heading,
+                    heading.right(),
+                    heading.right().right()
+            ));
+        }
+    }
 
     RailHeading(int offsetX, int offsetZ, int angle) {
         this.offsetX = offsetX;
         this.offsetZ = offsetZ;
         this.angle = angle;
+    }
+
+    /**
+     * List of possible headings that the current
+     * heading can proceed in based on the supported
+     * headings. Use this to define possible paths
+     * and curves for the rails.
+     * <p>
+     * This list is immutable and can not be changed. If
+     * you need to support more angles or directions
+     * create logic in the snap system for more values.
+     *
+     * @return immutable list of headings
+     */
+    public List<RailHeading> getPossibleHeadings() {
+        return possibleHeadings;
+    }
+
+    /**
+     * Gets the next heading to the left. This
+     * will increment by 45 on a compass rose.
+     *
+     * @return heading to the left
+     */
+    public RailHeading left() {
+        return byIndexWrapped(ordinal() - 1);
+    }
+
+    /**
+     * Gets the next heading to the right. This
+     * will increment by 45 on a compass rose.
+     *
+     * @return heading to the right
+     */
+    public RailHeading right() {
+        return byIndexWrapped(ordinal() + 1);
+    }
+
+    /**
+     * Gets the heading at the index. Wraps the value
+     * if it is below or above the range of the enum.
+     *
+     * @param index - index of the heading, or index to wrap
+     * @return heading
+     */
+    public static RailHeading byIndexWrapped(int index) {
+        return byIndex(MathHelpers.wrap(index, values().length - 1));
+    }
+
+    /**
+     * Gets the heading by index.
+     * <p>
+     * Values that fall outside the range will default
+     * to {@link #NORTH} and print an error to console
+     * with trace.
+     *
+     * @param index - index of the heading
+     * @return heading
+     */
+    public static RailHeading byIndex(int index) {
+        if (index >= 0 && index < values().length) {
+            return values()[index];
+        }
+
+        //TODO replace with Log4j2
+        System.out.println("Error: index '" + index + "' could not be matched to a heading.");
+        new RuntimeException("trace").printStackTrace();
+
+        return NORTH;
     }
 
     /**
@@ -46,7 +144,10 @@ public enum RailHeading {
                 return heading;
             }
         }
+        //TODO replace with Log4j2
         System.out.println("Error: angle '" + angleInput + "' could not be matched to a heading.");
+        new RuntimeException("trace").printStackTrace();
+
         return RailHeading.NORTH;
     }
 }
